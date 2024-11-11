9c2535a64a05b1813ecf7741b8844caf95415256
Chaman Chaudhary <chaudharychaman1506@gmail.com>
2024-11-11 23:43:28 +0530

    add dialog box for deletion

--- src/components/custom/CustomerVendor/DeleteButton.jsx
+++ src/components/custom/CustomerVendor/DeleteButton.jsx
@@ -1,11 +1,25 @@
 "use client";
 
 import { deleteCustomerVendor } from "@/actions/CustomerVendor";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { Trash2Icon } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const DeleteButton = ({ id }) => {
+  const [open, setOpen] = useState(false);
   const router = useRouter();
+
   const handleDelete = async () => {
     const res = await deleteCustomerVendor(id);
     if (res) {
@@ -13,7 +27,38 @@ const DeleteButton = ({ id }) => {
     }
   };
 
-  return <Trash2Icon onClickCapture={handleDelete} className="text-red-500" />;
+  return (
+    <>
+      <Trash2Icon className="text-red-500" onClick={() => setOpen(true)} />
+      <AlertDialogBox
+        open={open}
+        setOpen={setOpen}
+        handleDelete={handleDelete}
+      />
+    </>
+  );
 };
 
 export default DeleteButton;
+
+const AlertDialogBox = ({ open, setOpen, handleDelete }) => {
+  return (
+    <AlertDialog open={open} setOpen={setOpen}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are You Sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            You're about to delete this item. This action is permanent and
+            cannot be undone. Do you wish to proceed?
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel onClick={() => setOpen(false)}>
+            Cancel
+          </AlertDialogCancel>
+          <AlertDialogAction onClick={handleDelete}>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
